abd73681e75f1e72d7e955b27455b221ca622e60
cmillecan <cmillecan@gmail.com>
2020-02-06 02:46:23 +0000

    add file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - blah blah blah
+* Return: blah blah blah
+*/
+int main(void)
+{
+printf("size of a char: %d\n", (int)sizeof(char));
+printf("size of an int: %d\n", (int)sizeof(int));
+printf("size of a long int: %d\n", (int)sizeof(long));
+printf("size of a float: %d\n", (int)sizeof(float));
+return (0);
+}
