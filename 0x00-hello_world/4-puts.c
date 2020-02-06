f7d35ccaaba6d0ea2633f98e16ca49a91b9a90f5
cmillecan <cmillecan@gmail.com>
2020-02-06 01:03:25 +0000

    add file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
 * main - blah blah blah
 * Return: blah blah blah
 */
 int main(void)
 {
-	puts("\"Programming is like a building a multilingual puzzle");
-
-	return (0);
+puts("\"Programming is like a building a multilingual puzzle");
+return (0);
 }
