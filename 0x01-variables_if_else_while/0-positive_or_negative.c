c98894f4ef9c4f94556ef9d0895621278de0416a
cmillecan <cmillecan@gmail.com>
2020-02-06 20:26:56 +0000

    add file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
+/**
+* main - this is a description for main
+* Return: integer
+*/
 int main(void)
 {
 int n;
