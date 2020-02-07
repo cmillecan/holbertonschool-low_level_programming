7b0fa272b5b0805bb2378330c8333a6f83d28cc6
cmillecan <cmillecan@gmail.com>
2020-02-07 03:13:01 +0000

    add file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - entry point
+* Description: description for main function
+* Return: Always return 0
+*/
+int main(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+{
+printf("%d", n);
+}
+printf("\n");
+return (0);
+}
