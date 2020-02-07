5e17b6158ee22ed140667273faa4aa52ce3009c5
cmillecan <cmillecan@gmail.com>
2020-02-07 02:25:35 +0000

    add file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - entry point
+* Description: description for main function
+* Return: Always return 0
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+for (ch = 'A'; ch <= 'Z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
