eda408bc3e2d4a7702b215099651e05ee5d81dbd
cmillecan <cmillecan@gmail.com>
2020-02-07 04:30:41 +0000

    add file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - entry point
+* Description: description for main function
+* Return: Always return 0
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
