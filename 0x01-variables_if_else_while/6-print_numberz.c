08143f6b3764a9c257b046bf1e51d1f91472a970
cmillecan <cmillecan@gmail.com>
2020-02-07 03:36:56 +0000

    add file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - entry point
+* Description: description for main function
+* Return: Always return 0
+*/
+int main(void)
+{
+int ch;
+for (ch = '0'; ch <= '9'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
