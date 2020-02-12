8d88efad045a85da86d2cd02868baa3276b7a599
cmillecan <cmillecan@gmail.com>
2020-02-12 02:41:03 +0000

    add file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+* add - adds two digits
+* @a: is first argument
+* @b: is the second argument
+* Return: Always 0
+*/
+
+int add(int a, int b)
+{
+
+	int c;
+
+	c = a + b;
+
+	_putchar(c);
+
+	return (0);
+}
