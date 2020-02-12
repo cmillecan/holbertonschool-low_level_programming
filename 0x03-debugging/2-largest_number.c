e5866c7db53f2db8dcf9cd4173be96593e92e3b0
cmillecan <cmillecan@gmail.com>
2020-02-12 04:04:17 +0000

    add file

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,17 +12,9 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a > b && a > c)
 	{
 		largest = a;
-	}
-	else if (b > a && a > c)
-	{
-		largest = b;
-	}
-	else
-	{
-		largest = c;
 	}
 
 	return (largest);
