89adc05dd25009dd68552fc398744d9ac99a6b7e
cmillecan <cmillecan@gmail.com>
2020-02-13 15:08:26 -0800

    add file

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+* more_numbers - print 10 times the numbers
+* @void: is the argument
+* Return: 0
+*/
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+			{
+				_putchar('0' + j / 10);
+			}
+			_putchar('0' + j % 10);
+		}
+		_putchar('\n');
+	}
+}
