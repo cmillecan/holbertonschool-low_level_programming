e789c7bd6a7ee5ec53b5fcc149eb558b26fdfb8a
cmillecan <cmillecan@gmail.com>
2020-02-13 18:08:36 -0800

    add file

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+* print_diagonal - draw a diagonal line
+* @n: is first argument
+* Return: void
+*/
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+                _putchar('\n');
+	}
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}
