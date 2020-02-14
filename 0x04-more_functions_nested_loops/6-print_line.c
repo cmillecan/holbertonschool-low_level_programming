98aff8e2ec974933ab8569acdcf922d26e6e80f2
cmillecan <cmillecan@gmail.com>
2020-02-13 17:16:53 -0800

    add file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+* print_line - draw a straight line
+* @n: is first argument
+* Return: void
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
