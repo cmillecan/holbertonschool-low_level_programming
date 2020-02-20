35a888c5c65d516997348eb4aed6f849d4bfd246
cmillecan <cmillecan@gmail.com>
2020-02-20 12:42:09 -0800

    add file

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+* string_toupper - changes lowercase letters to uppercase
+* @s: is the string
+* Return: char
+*/
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+	return (s);
+}
