1b1d1872d30e1d0b661b47781396cc0d9b2879ab
cmillecan <cmillecan@gmail.com>
2020-02-20 19:28:17 -0800

    add file

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,6 +14,10 @@ char *cap_string(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] -= 32;
+		}
 		for (j = 0; a[j] != '\0'; j++)
 		{
 			if (s[i] == a[j])
