d2cb1232b8eac570ef14f60f2088b41775aaf309
cmillecan <cmillecan@gmail.com>
2020-02-24 12:08:16 -0800

    add files

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+* _strspn - gets the length of a prefix substring
+* @s: first argument
+* @accept: second argument
+* Return: char
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (accept[j] == s[i])
+				break;
+		}
+		if (!accept[j])
+			break;
+	}
+	return (i);
+}
