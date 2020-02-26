dca8607182ee0cf3300f2b2154a441af53a04b97
cmillecan <cmillecan@gmail.com>
2020-02-26 14:56:52 -0800

    add file

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,14 +8,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	_putchar(*s++);
-
-	if (*s != '\0')
+	if (*s)
 	{
-		_print_rev_recursion(s);
-	}
-	else
-	{
-		_putchar('\n');
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
 }
