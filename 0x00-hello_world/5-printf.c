408496d66a46356c83d45ffa64713f97af192c36
cmillecan <cmillecan@gmail.com>
2020-03-02 18:08:23 +0000

    updated description

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
-* main - blah blah blah
-* Return: blah blah blah
+* main - prints a string
+* Return: 0
 */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
 }
