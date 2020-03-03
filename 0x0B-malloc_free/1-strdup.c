1fe3a59af733cab30bedc8a6d7127a24972b4a4c
cmillecan <cmillecan@gmail.com>
2020-03-03 23:14:49 +0000

    add file 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,6 +16,7 @@ char *_strdup(char *str)
 	int i;
 
 	len = 0;
+
 	if (str == NULL)
 	{
 		return (NULL);
@@ -28,6 +29,10 @@ char *_strdup(char *str)
 
 	cpy = malloc(sizeof(char) * (len + 1));
 
+	if (cpy == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < len; i++)
 	{
 		cpy[i] = str[i];
