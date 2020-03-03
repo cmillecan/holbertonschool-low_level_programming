2c6c1f52c8b1bf6be5954c792a734f3aa10101a8
cmillecan <cmillecan@gmail.com>
2020-03-03 22:04:47 +0000

    add file 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,15 +17,13 @@ char *create_array(unsigned int size, char c)
 	s = malloc(sizeof(char) * size);
 
 	if (size == 0)
-        {
-                return (NULL);
-        }
-	
+	{
+	return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
 		s[i] = c;
 	}
-	
 	if (s == NULL)
 	{
 		return (NULL);
