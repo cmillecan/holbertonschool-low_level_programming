44dd8d3b61f86ab9900b7882caa24a4e9fd5c665
cmillecan <cmillecan@gmail.com>
2020-03-04 04:18:57 +0000

    add file 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -30,12 +30,12 @@ char *str_concat(char *s1, char *s2)
 	}
 
 	k = 0;
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; s1 != NULL && s1[i] != '\0'; i++)
 	{
 		s3[k] = s1[i];
 		k++;
 	}
-	for (j = 0; s2[j] != '\0'; j++)
+	for (j = 0; s2 != NULL && s2[j] != '\0'; j++)
 	{
 		s3[k] = s2[j];
 		k++;
