abb4e9a0a75aeda7094912004f14acd74867de63
cmillecan <cmillecan@gmail.com>
2020-03-06 20:17:08 +0000

    add file 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,77 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned int _strlen(char *s);
+
+/**
+* _strlen - returns the length of a string
+* @s: is a string
+* Return: length of string
+*/
+
+unsigned int _strlen(char *s)
+{
+	unsigned int c = 0;
+
+	while (s[c] != '\0')
+		c++;
+
+	return (c);
+}
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: unsigned int
+ * Return: 0
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+	char *s3;
+	unsigned int i, j;
+	unsigned int count, len1, len2;
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+
+	if (s1 == NULL)
+	{
+		len1 = 0;
+	}
+	if (s2 == NULL)
+	{
+		len2 = 0;
+	}
+	if (n >= len2)
+	{
+		n = len2;
+	}
+
+	s3 = malloc(sizeof(char) * (len1 + n + 1));
+	if (s3 == NULL)
+	{
+		return (NULL);
+	}
+	if (s1)
+	{
+		for (i = 0; i < len1; i++, count++)
+		{
+			s3[count] = s1[i];
+		}
+	}
+	if (s2)
+	{
+		for (j = 0; j < n; j++, count++)
+		{
+			s3[count] = s2[j];
+		}
+	}
+
+	s3[count] = '\0';
+
+	return (s3);
+}
