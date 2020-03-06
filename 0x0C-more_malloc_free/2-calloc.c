3349205f0004b4b75c7146413c6e3813510d9ea9
cmillecan <cmillecan@gmail.com>
2020-03-06 05:30:31 +0000

    add file 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -33,15 +33,16 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
 
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
 	ptr = malloc(nmemb * size);
 
 	if (ptr == NULL)
 	{
 		return (NULL);
-	}
-	if (nmemb == 0 || size == 0)
-	{
-	return (NULL);
 	}
 
 	_memset(ptr, 0, (nmemb * size));
