72a9b9d396da47fa05ed53fcc0db61180459ff4b
cmillecan <cmillecan@gmail.com>
2020-03-09 19:35:31 +0000

    add file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,7 +17,23 @@ struct dog
 	char *owner;
 };
 
+/**
+ * struct dog_t - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+
+typedef struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+
 void init_dog(struct dog, char, float, char);
 void print_dog(struct dog *d);
-
 #endif
