f60af72b4f54b4c7a55c866de246c4ba052cc8b9
cmillecan <cmillecan@gmail.com>
2020-03-09 19:28:21 +0000

    add header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog, char, float, char);
+void print_dog(struct dog *d);
+
+#endif
