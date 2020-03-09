5a689890d5346abdddc82fda4bbe35a119e9f491
cmillecan <cmillecan@gmail.com>
2020-03-09 19:13:55 +0000

    add file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
+#include "holberton.h"
 
 /**
  * main - prints the name of the file it was compiled from
  * Return: int
  */
 
-int main()
+int main(void)
 {
 
-	printf("%s\n", __FILE__ );
+	printf("%s\n", __FILE__);
 
 	return (0);
 }
