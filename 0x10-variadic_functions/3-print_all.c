0d6a404de6fade3df028aa4556b3600ae435fe6c
cmillecan <cmillecan@gmail.com>
2020-03-14 23:28:13 +0000

    add file 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -16,7 +16,7 @@ void print_all(const char * const format, ...)
 
 	va_start(optArgs, format);
 	i = 0;
-	while (format[i] != '\0')
+	while (format != NULL && format[i] != '\0')
 	{
 		switch (format[i])
 		{
