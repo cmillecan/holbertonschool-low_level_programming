5167a166e4b7ed29a857467139ba664fbc7b0137
cmillecan <cmillecan@gmail.com>
2020-03-20 06:34:58 +0000

    add file 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,15 +1,23 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * free_list - frees list_t
  * @head: head
  * Return: void
- *
- * Description: singly linked list node structure
- * for Holberton project
  */
 
 void free_list(list_t *head)
 {
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+
+		free(head->str);
+		free(head);
+		head = temp;
+	}
 }
