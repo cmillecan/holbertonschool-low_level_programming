86e099df6e1671675e32b508767263dd55412c76
cmillecan <cmillecan@gmail.com>
2020-03-20 06:24:24 +0000

    add file

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * free_list - frees list_t
+ * @head: head
+ * Return: void
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+
+void free_list(list_t *head)
+{
+}
