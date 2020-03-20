c5532dca84e96ba2ff0af6f408324f7a4aa68160
cmillecan <cmillecan@gmail.com>
2020-03-20 02:59:37 +0000

    add file 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the list
+ * Return: number of elements
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+size_t list_len(const list_t *h)
+{
+        list_t *currentNode;
+        size_t total = 0;
+
+        if (h == NULL)
+        {
+                return (0);
+        }
+        total++;
+
+        currentNode = h->next;
+        while (currentNode != NULL)
+        {
+                total++;
+                currentNode = currentNode->next;
+        }
+
+        return (total);
+}
