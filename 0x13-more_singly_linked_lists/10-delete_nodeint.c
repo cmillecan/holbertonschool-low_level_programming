0d0e41aaa276938509d7162d231cf84817d9c6be
cmillecan <cmillecan@gmail.com>
2020-03-25 05:38:17 +0000

    add file

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *temp = *head, *next;
 	unsigned int i;
 
-	if (head != NULL && *head == NULL)
+	if (head == NULL && *head == NULL)
 	{
 		return (-1);
 	}
