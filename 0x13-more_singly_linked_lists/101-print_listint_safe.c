b78bbb244eb17a2a528f617609080783c1ff490c
cmillecan <cmillecan@gmail.com>
2020-03-25 06:51:35 +0000

    add file

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * print_listint_safe - prints a linked list
+ * @head: head
+ * Return: number of nodes
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	listint_t *currentNode;
+	size_t total = 0;
+
+	if (head == NULL)
+	{
+		exit(98);
+	}
+	total++;
+
+	currentNode = head->next;
+	while (currentNode != NULL)
+	{
+		printf("%s\n", currentNode);
+		total++;
+		currentNode = currentNode->next;
+	}
+
+	return (total);
+}
