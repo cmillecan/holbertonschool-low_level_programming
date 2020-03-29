739e20d73895f89bae8eaf5de43563afc0b0efbd
cmillecan <cmillecan@gmail.com>
2020-03-29 03:54:10 +0000

    add file

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * print_binary - prints the binary representation of a number
@@ -8,9 +9,30 @@
  */
 void print_binary(unsigned long int n)
 {
+	unsigned long int rev = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
 	while (n > 0)
 	{
-		if (n == (n >> 1) << 1)
+		if (1 == (n & 1))
+		{
+			rev = (rev << 1) + 1;
+		}
+		else
+		{
+			rev = (rev << 1);
+		}
+		n = n >> 1;
+	}
+
+	while (rev > 0)
+	{
+		if (1 == (rev & 1))
 		{
 			_putchar('1');
 		}
@@ -18,6 +40,6 @@ void print_binary(unsigned long int n)
 		{
 			_putchar('0');
 		}
-		n = n >> 1;
+		rev = rev >> 1;
 	}
 }
