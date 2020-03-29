8946749590baec9dcfb3bae8573cbf89134e7475
cmillecan <cmillecan@gmail.com>
2020-03-29 04:58:54 +0000

    add file

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,13 +10,14 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	if (index >= 64)
+	{
+		return (-1);
+	}
 	if ((n & (1 << index)) == (unsigned long int)(1 << index))
 	{
 		return (1);
 	}
-	if ((n & (1 << index)) == 0)
-	{
-		return (0);
-	}
-	return (-1);
+
+	return (0);
 }
