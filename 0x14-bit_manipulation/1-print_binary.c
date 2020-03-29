f70d239e52a41c889cb69d4d01832eb589e3e43d
cmillecan <cmillecan@gmail.com>
2020-03-29 04:17:39 +0000

    add file

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,37 +9,9 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int rev = 0;
-
-	if (n == 0)
+	if (n > 1)
 	{
-		_putchar('0');
-		return;
+		print_binary(n >> 1);
 	}
-
-	while (n > 0)
-	{
-		if (1 == (n & 1))
-		{
-			rev = (rev << 1) + 1;
-		}
-		else
-		{
-			rev = (rev << 1);
-		}
-		n = n >> 1;
-	}
-
-	while (rev > 0)
-	{
-		if (1 == (rev & 1))
-		{
-			_putchar('1');
-		}
-		else
-		{
-			_putchar('0');
-		}
-		rev = rev >> 1;
-	}
+	_putchar((n & 1) + '0');
 }
