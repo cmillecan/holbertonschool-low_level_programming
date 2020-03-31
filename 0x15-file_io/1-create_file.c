8ad56f68856e580833e82b3b5567e46ce9458592
cmillecan <cmillecan@gmail.com>
2020-03-31 19:45:16 +0000

    add file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,5 +9,31 @@
  */
 int create_file(const char *filename, char *text_content)
 {
+	int fd, i, writenum;
 
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+
+	for (i = 0; text_content[i] != '\0'; i++)
+	{
+	}
+
+	writenum = write(fd, text_content, i);
+
+	if (writenum == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
 }
