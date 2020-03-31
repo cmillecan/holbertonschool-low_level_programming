1d604a99d39aed94f735a7c142ec27dc2bdc6acf
cmillecan <cmillecan@gmail.com>
2020-03-31 19:49:51 +0000

    add file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,5 +9,31 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int fd, i, writenum;
 
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+
+	for (i = 0; text_content[i] != '\0'; i++)
+	{
+	}
+
+	writenum = write(fd, text_content, i);
+
+	if (writenum == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
 }
