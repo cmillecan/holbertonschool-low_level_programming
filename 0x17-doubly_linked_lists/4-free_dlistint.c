5d470633546b03560325eeb7887b9ba3b671ffae
cmillecan <cmillecan@gmail.com>
2020-05-03 20:06:54 +0000

    add file

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+  * free_dlistint - frees a dlistin_t list
+  *
+  * @head: head
+  * Return: void
+*/
+void free_dlistint(dlistint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+
+		free(head->next);
+		free(head);
+		head = temp;
+	}
+}
