3e808bf085a9a34007fe2dff1a3b4e327a729c8f
cmillecan <cmillecan@gmail.com>
2020-05-03 17:54:26 +0000

    add file

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,23 +8,4 @@
 */
 size_t print_dlistint(const dlistint_t *h)
 {
-	dlistint_t *currentNode;
-	size_t total = 0;
-
-	if (h == NULL)
-	{
-		return (0);
-	}
-	printf("%d\n", h->n);
-	total++;
-
-	currentNode = h->next;
-	while (currentNode != NULL)
-	{
-		printf("%d\n", currentNode->n);
-		total++;
-		currentNode = currentNode->next;
-	}
-
-	return (total);
 }
