772d8ffe092b56462b3ee26f500624d5d4e5fc28
cmillecan <cmillecan@gmail.com>
2020-05-04 01:42:09 +0000

    add file

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -23,7 +23,13 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 
 	for (i = 0; temp != NULL && i < index - 1; i++)
+	{
 		temp = temp->next;
+		if (temp == NULL)
+		{
+			return (-1);
+		}
+	}
 
 	if (temp == NULL || temp->next == NULL)
 		return (-1);
