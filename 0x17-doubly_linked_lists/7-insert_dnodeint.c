2be537f65680f9f9fdb2e129d5a0360cd573a540
cmillecan <cmillecan@gmail.com>
2020-05-04 01:28:48 +0000

    add file

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -33,7 +33,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	for (; count < idx; count++)
 	{
 		currentNode = currentNode->next;
-		return (newNode);
+		if (currentNode == NULL)
+		{
+			free(newNode);
+			return (NULL);
+		}
 	}
 	newNode->prev = currentNode;
 	newNode->next = currentNode->next;
